3cf54604f4c36d0250461412995d44467006f936
Thomas Fahey <thomasfahey@mail.com>
2025-05-04 12:56:28 +0100

    adsf

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ export default {
     // existing config...
     build: {
       rollupOptions: {
-        input: {
-          main: 'index.html',
-          admin: 'public/admin/index.html'  // NEW!
-        }
+        input: 'index.html'
       }
     }
   }
