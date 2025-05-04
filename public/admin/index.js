de6cc24702ae147fe80e1f07910605821a2d7925
Thomas Fahey <thomasfahey@mail.com>
2025-05-04 12:51:10 +0100

    Remove old files.

--- public/admin/index.js
+++
@@ -1,8 +0,0 @@
-import CMS from "decap-cms";
-
-// If you need custom widgets or previews, you’d import or register them here
-
-// Optionally, register preview templates or custom widgets
-
-// Finally, initialize the CMS (it will read your config.yml automatically)
-CMS.init();
