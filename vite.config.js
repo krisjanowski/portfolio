7bcd5fc5a198106da537db6313f431b77bc9bafe
Thomas Fahey <thomasfahey@mail.com>
2025-05-04 13:18:34 +0100

    asdfsaf

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,8 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
-
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        admin: 'public/admin/index.html'
+export default {
+    // existing config...
+    build: {
+      rollupOptions: {
+        input: 'index.html'
       }
     }
-  },
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/admin/config.yml',
-          dest: 'admin'
-        }
-      ]
-    })
-  ]
-});
+  }
