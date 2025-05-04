dd7ccc0e4f9123f3b5a40ee32ff091bdc2e665ef
Thomas Fahey <thomasfahey@mail.com>
2025-05-04 12:13:44 +0100

    No longer using CDN.

---
+++ public/admin/index.js
@@ -0,0 +1,9 @@
+import CMS from "decap-cms";
+import "decap-cms/dist/decap-cms.css";
+
+// If you need custom widgets or previews, you’d import or register them here
+
+// Optionally, register preview templates or custom widgets
+
+// Finally, initialize the CMS (it will read your config.yml automatically)
+CMS.init();
